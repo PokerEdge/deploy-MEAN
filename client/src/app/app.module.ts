dc169a6e711e8b881e618216a2c04807c273d188
Reid <Reid@pokersprout.com>
2019-01-11 18:15:39 -0500

    First commit
    First commit
    
    Set up beginnings of Angular front end with Material and beginning work
    to set up Node.js server.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatToolbarModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
